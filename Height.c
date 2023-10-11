3565264fb528a455e44477e83433f6f8d49d13d6
venkata reddy <147414319+venkat0127@users.noreply.github.com>
2023-10-11 10:43:30 +0530

    #include<stdio.h> #include<math.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     if(a>b)     {         printf("%d",a);     }     else     {         printf("%d",b);     } }

---
+++ Height.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if(a>b)
+    {
+        printf("%d",a);
+    }
+    else
+    {
+        printf("%d",b);
+    }
+}
