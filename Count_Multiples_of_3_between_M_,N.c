cc76f46c6c6ff576465ea428e55b2c011bf74a64
venkata reddy <147414319+venkat0127@users.noreply.github.com>
2023-11-06 10:56:33 +0530

    #include<stdio.h> int main() {     int a,b,i,n=0;     scanf("%d%d",&a,&b);     for(i=a;i<=b;i++)     {         if(i%3==0)         {             n++;         }     }     printf("%d",n); }

---
+++ Count_Multiples_of_3_between_M_,N.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,n=0;
+    scanf("%d%d",&a,&b);
+    for(i=a;i<=b;i++)
+    {
+        if(i%3==0)
+        {
+            n++;
+        }
+    }
+    printf("%d",n);
+}
