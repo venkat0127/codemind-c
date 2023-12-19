a9bbce4119f308650b3226e46fd73ee91fe06672
venkata reddy <147414319+venkat0127@users.noreply.github.com>
2023-12-19 10:15:21 +0530

    #include<stdio.h> int main() {     int i,n,sum;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     {         for(i=0;i<n;i++)     if(arr[i]%2==0)     sum+=arr[i];     printf("%d",sum);     } }

---
+++ Even_Sum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,sum;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    {
+        for(i=0;i<n;i++)
+    if(arr[i]%2==0)
+    sum+=arr[i];
+    printf("%d",sum);
+    }
+}
