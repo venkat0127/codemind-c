acf447fe9d4e4c0e58f0b4c77e7bf512f77e854f
venkata reddy <147414319+venkat0127@users.noreply.github.com>
2023-12-22 09:05:57 +0530

    #include<stdio.h> int main() {     int a,b,c,d,e,f,g;     scanf("%d%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f,&g);     if((c-d)>=b)     {         printf("YES ");     }     else     {         printf("NO ");     }     if((f-g)>=e)     {         printf("YES ");     }     else     {         printf("NO ");     } }

---
+++ Student_Registration.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,e,f,g;
+    scanf("%d%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f,&g);
+    if((c-d)>=b)
+    {
+        printf("YES
+");
+    }
+    else
+    {
+        printf("NO
+");
+    }
+    if((f-g)>=e)
+    {
+        printf("YES
+");
+    }
+    else
+    {
+        printf("NO
+");
+    }
+}
